46f7fc9c4e42a3ee6a18c4fe25ac9029a9367f93
nbharats <bsn41003@gmail.com>
2026-04-03 10:50:52 +0530

    resolved razorpay issues and data storage

--- app.py
+++ app.py
@@ -414,14 +414,14 @@ def campaigndetails(campaignid):
             camps=cursor.fetchone()
             cursor.execute('select sum(raised_amount) from campaigns where campaigns.id = %s',[campaignid])
             total=cursor.fetchone()[0]
-            print('total',total/camps[3])
+            # print('total',total/camps[3])
             cursor.execute('select id,email from users where name=%s or email=%s',[session.get('user'), session.get('user')])
             userlog=cursor.fetchone()
             cursor.close()
         except Exception as e :
             print(e)
             flash('campaign Could not retrive details')
-            return redirect(url_for('campaigndetails'))
+            return redirect(url_for('campaigndetails',campaignid=campaignid))
         # print('campaignid',campaignid)
         else:
             if request.method=='POST':
@@ -429,7 +429,7 @@ def campaigndetails(campaignid):
                 d_name=request.form['donor_name']
                 d_email=request.form['donor_email']
                 d_phno=request.form['donor_phone']
-                print(d_amount,d_name,d_email,d_phno)
+                # print(d_amount,d_name,d_email,d_phno)
 
                 session[session.get('user')][campaignid]=[d_name,
                                               d_email,
@@ -439,7 +439,7 @@ def campaigndetails(campaignid):
                                               userlog[0]
                 ]
                 ses=session[session.get('user')][campaignid]
-                print(ses[3])
+                # print(ses[3])
                 flash('message sent')
                 return redirect(url_for('donation_pay',campaignid=campaignid))
             else:
@@ -451,7 +451,7 @@ def campaigndetails(campaignid):
 
 @app.route('/donation_pay/<campaignid>',methods=['GET','POST'])
 def donation_pay(campaignid):
-    print("campaignid",campaignid)
+    # print("campaignid",campaignid)
     if session.get('user'):
         try:
             cursor=database.cursor(buffered=True)
@@ -459,18 +459,18 @@ def donation_pay(campaignid):
             userlog=cursor.fetchone()[0]
             cursor.close()
             ses=session[session.get('user')][campaignid]
-            print(ses)
+            # print(ses)
             amount=int(float(ses[3])+(float(ses[3])*0.2))
             razor_amount=amount*100
-            print(razor_amount)
+            # print(razor_amount)
             order=client.order.create({
                 'amount':razor_amount,
                 "currency": "INR",
                 'receipt':f"{ses[0]}",
                 'payment_capture':'1'
             })
-            print(order)
-            return render_template('donation_pay.html',order=order,client=ses)
+            # print(order)
+            return render_template('donation_pay.html',campaignid=campaignid,order=order,client=ses)
         except Exception as e :
             print(e)
             flash('donation Could not retrive details',category='danger')
@@ -480,18 +480,19 @@ def donation_pay(campaignid):
         flash('Please Sign in to Proceed',category='danger')
         return redirect(url_for('userlogin'))
 
-@app.route('/success_donation')
-def success_donation():
+@app.route('/success_donation/<campaignid>',methods=['POST'])
+def success_donation(campaignid):
     try:
+        # print(request.form)
         pay_id=request.form['razorpay_payment_id']
         order_id=request.form['razorpay_order_id']
         sign=request.form['razorpay_signature']
         amount=request.form['grand_total']
 
         dic={
-            'razor_pay_id':pay_id,
-            'razor_order_id':order_id,
-            'razor_sign':sign
+            'razorpay_payment_id':pay_id,
+            'razorpay_order_id':order_id,
+            'razorpay_signature':sign
         }
         payment='paid'
         try:
@@ -500,21 +501,27 @@ def success_donation():
             print(e)
             flash('payment verification failed')
             payment='failed'
-            return redirect(url_for('home'))
+            return redirect(url_for('index'))
         else:
             donation_data=session.get(session.get('user'))
+            print(donation_data,'\n',donation_data.get(campaignid))
+            # donation_data=donation_data.get('campaignid')
+            # print(donation_data)
+            
             try:
                 cursor=database.cursor(buffered=True)
-                cursor.execute('insert into donations(razorpay_payment_id,razorpay_order_id,razorpay_signature,amount,donor_name,donor_email,donor_phone,campaign_id,status,ngo_id,user_id)',[pay_id,order_id,sign,donation_data[3],donation_data[0],donation_data[1],donation_data[2],donation_data[4],payment,donation_data[5],donation_data[6]])
+                cursor.execute('insert into donations(razorpay_payment_id,razorpay_order_id,razorpay_signature,amount,donor_name,donor_email,donor_phone,campaign_id,status,ngo_id,user_id) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',[pay_id,order_id,sign,donation_data.get(campaignid)[3],donation_data.get(campaignid)[0],donation_data.get(campaignid)[1],donation_data.get(campaignid)[2],donation_data.get(campaignid)[4],payment,donation_data.get(campaignid)[5],donation_data.get(campaignid)[6]])
                 database.commit()
                 cursor.close()
-                return redirect(url_for('home'))
+                print('data stored')
+                # return redirect(url_for('index'))
             except Exception as e:
-                print(e)
+                print('exception',e)
                 flash('Could not store details')
-                return redirect(url_for('campaigndetails'))
+                return redirect(url_for('index'))
             # if session.get(session.get('user')):
             #     donation_data=session.pop(session.get('user'))
+            return redirect(url_for('index'))
     except Exception as e:
         print(e)
         app.logger.exception(f'Payment verification failed {e}')
