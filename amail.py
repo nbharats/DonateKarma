b61fe202e265929abb306de28af8aa12f12f545b
TanarapuChakrapani <tanarapuchakrapani9491@gmail.com>
2026-04-04 16:32:59 +0530

    admin dashboard and invoice through mail completed

--- amail.py
+++ amail.py
@@ -14,3 +14,19 @@ def send_mail(subject,to,body):
     server.send_message(msg=msg)
     server.close()
 
+def send_invoice_mail(subject,to,body,attachment,filename):
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('bsn41003@gmail.com',app)
+    msg=EmailMessage()
+    msg['TO']=to
+    msg['FROM']='bsn41003@gmail.com'
+    msg['subject']=subject
+    msg.set_content(body)
+    msg.add_attachment(
+            attachment,
+            maintype='application',
+            subtype='pdf',
+            filename=filename
+        )
+    server.send_message(msg=msg)
+    server.close()
